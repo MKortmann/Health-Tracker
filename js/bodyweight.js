94f2063c7434b59bfe2664af75c9957088f385d9
Dell_MVMS <companymvms@gmail.com>
2019-07-29 17:12:54 +0200

    add: design / structure

---
+++ js/bodyweight.js
@@ -0,0 +1,12 @@
+// Basic sctructure: BLUE PRINT
+
+// (function() {
+//   // Declare private vars and functions
+//
+//   return {
+//     // Declare public var and functions
+//   }
+//
+// })();
+
+console.log("hello");
