7bd54c1393733d5d85dbe438a2b8644e1b4763f4
Dell_MVMS <companymvms@gmail.com>
2019-08-09 20:51:22 +0200

    bug: trying to fix the problem: $ is not defined

--- js/statepage.js
+++ js/statepage.js
@@ -240,7 +240,9 @@ const bodyWeight = function() {
   <br>
 
   <!-- Add tht js file -->
-  <script src="./js/bodyweight.js"></script>  
+  <script src="./js/bodyweight.js"></script>
+  <script src="./lib/jquery/jquery-3.3.1.js"></script>
+  <script src="./lib/bootstrap/bootstrap.js"></script>
 `;
 }
 ///////////////////////////////////////////////////////////////////////////////
