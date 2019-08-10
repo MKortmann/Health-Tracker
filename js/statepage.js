d7e6d961875c05f6fffa12a832b4a4be2b69cccd
Dell_MVMS <companymvms@gmail.com>
2019-08-10 06:18:50 +0200

    style: improved modal for delete all items

--- js/statepage.js
+++ js/statepage.js
@@ -168,6 +168,12 @@ const bodyWeight = function() {
   <div class="modal fade" id="locModal" tabindex="-1" role="dialog" aria-labelledby="locModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Delete All Items</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
         <div class="modal-body">
           <p>Are you sure that you want to delete all the items?</p>
         </div>
