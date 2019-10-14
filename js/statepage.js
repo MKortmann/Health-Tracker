cefd0c3d31090adfbd134ad19caf1db60e005e95
MKortmann <companymvms@gmail.com>
2019-10-14 22:17:31 +0200

    style: restyle homepage + refactor

--- js/statepage.js
+++ js/statepage.js
@@ -26,13 +26,24 @@ const PageState = function() {
 
   this.change = function(state) {
     // get the current and the future id of the nav element
-    const currentId = currentState.__proto__.constructor.name;
-    const newId = state.__proto__.constructor.name;
+    const currentElem = "." + currentState.__proto__.constructor.name;
+    const newElem = "." + state.__proto__.constructor.name;
     // Update the active state of the nav element
-    document.getElementById(currentId).classList.remove("active");
-    document.getElementById(newId).classList.add("active");
+    console.log(`CURRENT ELEMENT: ${currentElem}`);
+    console.log(`NEW ELEMENT: ${newElem}`);
+    document.querySelectorAll(currentElem).forEach((item) => {
+      console.log(`REMOVING THE ACTIVE FROM ${item}`);
+      item.classList.remove("active");
+    });
+    document.querySelectorAll(newElem).forEach((item) => {
+      console.log(`ADDING THE ACTIVE FROM ${item}`);
+      item.classList.add("active");
+    });
     // update state
     currentState = state;
+    // We need to reload the state as soon as the page reload!
+    EventListeners_Reload();
+
   }
   // return current state to be called outside
   this.returnState = function() {
@@ -54,14 +65,72 @@ const homeState = function() {
           <img src="imgs/background.jpg" class="img-fluid" id="backgroundOverview" alt="Background image">
           <!-- <img src="imgs/background10.jpg" id="background" class="img-fluid" alt="Responsive image"> -->
           <div class="card-img-overlay">
-            <div class="jumbotron mt-sm-5">
-              <h2 class=" text-info">Welcome to Health Tracker!</h2 >
-              <p class="lead">This app was done specially to help you to track and improve your health!</p>
-              <hr class="my-4">
-              <p>It is very important to check constantly our healthy. In this way, it is easier to detect any problem helping the doctors to make the correct diagnostic!  </p>
-              <p class="text-success"> Please visit regularly your doctor! </p>
-              <p class= "text-info text-center">&hearts; Thanks for using our app &hearts; </p>
+
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <!-- <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol> -->
+            <div class="carousel-inner">
+              <div class="carousel-item  active">
+                <div class="jumbotron w-100">
+                  <h2 class="text-info">Welcome to Health Tracker!</h2 >
+                  <p class="lead">This app was done specially to help you to track and improve your health!</p>
+                  <hr class="my-4">
+                  <p>It is very important to check constantly our healthy. In this way, it is easier to detect any problem helping the doctors to make the correct diagnostic!  </p>
+                  <!-- <p class="text-success"> Please visit regularly your doctor! </p> -->
+                  <p class= "text-info text-center">&hearts; Thanks for using our app &hearts; </p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="jumbotron  w-100">
+                  <h2 class="text-primary text-center mb-2">Body Weight</h2 >
+                  <img src="../imgs/weight.jpg" class="mx-auto d-block w-50" alt="weightImg">
+                  <!-- <div class="carousel-caption d-none d-md-block">
+                    <h4 mt-5>First slide label</h4>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </div> -->
+
+                  <h3 class="text-center mt-2 carousel-caption text-secondary d-none d-md-block"> easy way to track, analize and view your weight </h3>
+                  <div class="text-center">
+                    <button href="#" class="mt-3 btn btn-primary bodyWeight">Start Now</button>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="jumbotron w-100">
+                  <h2 class="text-primary text-center mb-2">Blood Pressure</h2 >
+                  <img src="../imgs/pressure.jpg" class="mx-auto d-block w-50" alt="...">
+                      <h3 class="text-center mt-2 carousel-caption text-secondary d-none d-md-block"> be smart and check your blood pressure </h3>
+                  <div class="text-center">
+                    <a href="#" class="mt-2 btn btn-primary bloodPressure">Start Now</a>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="jumbotron w-100">
+                  <h2 class="text-primary text-center mb-2">Blood Sugar</h2 >
+                  <img src="../imgs/onlysugar.jpg" class="mx-auto d-block w-50" alt="sugarImg">
+                  <h3 class="text-center mt-2 carousel-caption text-secondary d-none d-md-block"> be the boss and register your blood sugar  </h3>
+                  <div class="text-center">
+                    <a href="#" class="mt-2 btn btn-primary bloodSugar">Start Now</a>
+                  </div>
+                </div>
+              </div>
+
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+
           </div>
         </div>
       </div>
@@ -98,39 +167,52 @@ const mySettings = mySettingsImport;
 // Instantiate PageState
 const page = new PageState();
 
-// UI vars
-const overviewUI = document.getElementById("homeState"),
-  myDataUI = document.getElementById("myData"),
-  bodyWeightUI = document.getElementById("bodyWeight"),
-  bloodPressureUI = document.getElementById("bloodPressure"),
-  bloodSugarUI = document.getElementById("bloodSugar"),
-  settingsUI = document.getElementById("settings");
+function EventListeners_Reload () {
+  // UI vars
+  const overviewUI = document.querySelector(".homeState"),
+    myDataUI = document.querySelector(".myData"),
+    bodyWeightUI = document.querySelectorAll(".bodyWeight"),
+    bloodPressureUI = document.querySelectorAll(".bloodPressure"),
+    bloodSugarUI = document.querySelectorAll(".bloodSugar"),
+    settingsUI = document.querySelector(".settings");
 
-// EventListeners
-overviewUI.addEventListener("click", (e) => {
-  page.change(new homeState);
-  e.preventDefault();
-});
-myDataUI.addEventListener("click", (e) => {
-  page.change(new myData);
-  e.preventDefault();
-});
-bodyWeightUI.addEventListener("click", (e) => {
-  page.change(new bodyWeight);
-  // load the bodyWeight.js! The file will be load dinamically!!!
-  loadBodyWeight();
-  e.preventDefault();
-});
-bloodPressureUI.addEventListener("click", (e) => {
-  page.change(new bloodPressure);
-  e.preventDefault();
-});
-bloodSugarUI.addEventListener("click", (e) => {
-  page.change(new bloodSugar);
-  e.preventDefault();
-});
-settingsUI.addEventListener("click", (e) => {    
-  page.change(new mySettings);
-  settings();
-  e.preventDefault();
-});
+  // EventListeners
+  overviewUI.addEventListener("click", (e) => {
+    page.change(new homeState);
+    e.preventDefault();
+  });
+  myDataUI.addEventListener("click", (e) => {
+    page.change(new myData);
+    e.preventDefault();
+  });
+
+  bodyWeightUI.forEach(function(val) {
+    val.addEventListener("click", (e) => {
+      page.change(new bodyWeight);
+      // load the bodyWeight.js! The file will be load dinamically!!!
+      loadBodyWeight();
+      e.preventDefault();
+    });
+  });
+
+  bloodPressureUI.forEach( (item) => {
+    item.addEventListener("click", (e) => {
+      page.change(new bloodPressure);
+      e.preventDefault();
+    });
+  });
+
+  bloodSugarUI.forEach( (item) => {
+    item.addEventListener("click", (e) => {
+      page.change(new bloodSugar);
+      e.preventDefault();
+    });
+  });
+
+  settingsUI.addEventListener("click", (e) => {
+    page.change(new mySettings);
+    settings();
+    e.preventDefault();
+  });
+};
+EventListeners_Reload();
